d4e6e4c00399b5060ef379ccf1cfba69cbbaa089
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2024-04-27 01:46:19 +0530

    Added Codes

---
+++ JS-31-SettingUpFoldStruct/ToDoApiSetup/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {PORT, EMAIL} = require('./config/server.config');
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Started server at port: ${PORT} ${EMAIL}`);
+}) 
